7686202590994b102fabb8cb677e9b9c1dcd6b3f
qinglingM <mql951002@gmail.com>
2025-12-25 11:59:11 +0800

    chore: hide nextjs-portal in dev

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body data-env="$ENV{NODE_ENV}">{children}</body>
     </html>
   );
 }
